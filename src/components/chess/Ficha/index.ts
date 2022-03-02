865e984f79b3bf780aa229350690ba7c804ff20d
Simx72 <53983258+Simx72@users.noreply.github.com>
2022-03-01 19:26:38 -0500

    poniendo los datos de ficha en el namespace

--- src/components/chess/Ficha/index.ts
+++ src/components/chess/Ficha/index.ts
@@ -13,6 +13,52 @@ namespace Ficha {
    * El tipo de ficha
    */
   export enum Type { K, Q, B, H, R, P }
+
+  
+  /**
+   * Array con dos valores refiriendose
+   * a uno blano y el otro negro como
+   * el ying y el yang xd
+   */
+  interface BN<T> {
+    /**
+     * BLANCO
+     */
+    0: T;
+
+    /**
+     * NEGRO
+     */
+    1: T;
+  }
+  
+  /**
+   * Contiene los datos de una ficha
+   */
+  export interface DatosdeFicha {
+    svg: BN<string>
+  }
+
+  export const Fichas: { [P in Ficha.Type]: DatosdeFicha } = [
+    /* Ficha.Type.K */{
+      svg: ['blanca', 'negra']
+    },
+    /* Ficha.Type.Q */{
+      svg: ['blanca', 'negra']
+    },
+    /* Ficha.Type.B */{
+      svg: ['blanca', 'negra']
+    },
+    /* Ficha.Type.H */{
+      svg: ['blanca', 'negra']
+    },
+    /* Ficha.Type.R */{
+      svg: ['blanca', 'negra']
+    },
+    /* Ficha.Type.P */{
+      svg: ['blanca', 'negra']
+    },
+  ]
 }
 
 export class FichaO extends GameO { 
